c22e2781b912ecc2aa8c953a2a94e140640c1338
jonmes <mesfinjohn@rocketmail.com>
2022-06-26 11:21:43 -0700

    factors commit

---
+++ factors.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import math
+import sys
+
+filename = sys.argv[1]
+
+with open(filename, 'r', encode='utf-u') as f:
+    a = f.read()
+    a_l = a.count("\n")
+lines = 0
+b = 0
+with open(filename, 'r', encoding='utf-8') as f:
+    while lines  < a_l:
+        n1 = int (f.readline())
+        lines += 1
+        a = 2
+        while a < n1//2:
+            if math.fmod(n1, a) == 0:
+                print("{:d} = {:d} * {:d}".format(n1, a, int(n1/a), end=""))
+                a = n1
+                a +=1
